2c181c984991ebe7da132fd38b2af92b2b3f6a02
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-21 16:05:22 +0330

    install react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+  <div className='flex items-center jusitfy-center w-screen h-screen'>
+        <h1 className="text-green-600 mx-auto text-2xl font-bold"> My Games</h1>
+	</div>
+  );
+}
+
+export default App;
