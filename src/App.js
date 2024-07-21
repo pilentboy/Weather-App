d17f6858391d280e800788634b02662d55f30642
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-22 01:57:21 +0330

    test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './index.css'
 
 function App(){
@@ -6,5 +7,15 @@ function App(){
     
     </>
   )
+=======
+import './index.css';
+
+function App() {
+  return (
+  <div className='flex items-center jusitfy-center w-screen h-screen'>
+        <h1 className="text-green-600 mx-auto text-2xl font-bold"> My Games</h1>
+	</div>
+  );
+>>>>>>> parent of 9c6b3aa (test weather api)
 }
 export default App;
