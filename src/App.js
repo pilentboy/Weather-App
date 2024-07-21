68d00617970678202393464cef4e079d672545dc
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-22 01:59:48 +0330

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
+
 import './index.css'
 
-function App(){
-  return(
-    <>
-    
-    </>
-  )
-=======
-import './index.css';
 
 function App() {
   return (
@@ -16,6 +8,7 @@ function App() {
         <h1 className="text-green-600 mx-auto text-2xl font-bold"> My Games</h1>
 	</div>
   );
->>>>>>> parent of 9c6b3aa (test weather api)
 }
+
 export default App;
+
