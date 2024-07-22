7bddd89cb366cd3557fea03b63d69669ad93a2bb
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-23 01:26:20 +0330

    use ProtectedRoute for Result page

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,15 +1,9 @@
 
 import { useLocation } from "react-router-dom";
-import {useEffect } from "react";
 
 const Result = () => {
 
     const location=useLocation()
-
-    // useEffect(()=>{
-    //     if(!location.state) navigate('/')
-    // },[])
-
 
 
     return <h1>{location.state.name}</h1>
