85092d1fb06d6ea062a8ef04e1f78fa9a77f132b
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-22 21:45:43 +0330

    create container component

---
+++ src/components/container.jsx
@@ -0,0 +1,10 @@
+
+const Container = ({children}) => {
+    return (
+        <div className='w-screen h-screen bg-gray-900 '>
+            {children}
+        </div>  
+    );
+}
+
+export default Container;
