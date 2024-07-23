e62a1579b3a3a6869733b4d8998eac600f6fd045
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-23 23:00:47 +0330

    increase search input height on sm screens

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,7 +66,7 @@ const Home = () => {
 
 
            <form action='#' onSubmit={handleSearchLocation}>
-            <div className={`relative main-input w-72 h-10   ${selectedLan == 0 ? 'flex flex-row' : 'flex flex-row-reverse'} justify-between items-center animate__animated animate__infinite ${searching && 'animate__pulse'} bg-gray-800 md:w-96 md:h-16`}>
+            <div className={`relative main-input w-72 h-12   ${selectedLan == 0 ? 'flex flex-row' : 'flex flex-row-reverse'} justify-between items-center animate__animated animate__infinite ${searching && 'animate__pulse'} bg-gray-800 md:w-96 md:h-16`}>
 
                 {/* search location input*/}
               <input spellCheck type='text' value={location} title='search input'
