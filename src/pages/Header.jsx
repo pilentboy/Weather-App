7bfcb478aa956ef3c09bee4e1b25b549baf65955
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-24 00:34:34 +0330

    add history btn to header and change select lan style

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Select from 'react-select'
 import { useContext, useEffect } from "react";
 import {Settings} from '../context/settings'
+import { MdHistoryEdu } from "react-icons/md";
 
 
 const Header = () => {
@@ -11,18 +12,30 @@ const Header = () => {
    
     return (
         <>
-        <header className="w-screen container fixed top-0 left-[50%] translate-x-[-50%] px-4  my-4  flex bg-transparent z-[999] md:px-2">
+        <header className="w-screen container fixed top-0 left-[50%] translate-x-[-50%] px-4  my-4  flex items-center bg-transparent z-[999] md:px-2">
 
          <Select 
             onChange={e => handleChangeLan(e.value)}
              options={languages} 
                 defaultValue={languages[selectedLan]}
                 styles={{
-                    container: (provided) => ({
-                        ...provided
-                    })
+                    control: (provided) => ({
+                        ...provided,
+                        backgroundColor:'#9ca3af',
+                        borderColor:'#9ca3af',
+                        
+                    }),
+                    menuList: (provided) => ({
+                        ...provided,
+                        backgroundColor:'#9ca3af'
+                        
+                    })   
                 }}
             />
+
+            <button className="ms-3 cursor-pointer text-5xl text-gray-400 ">
+                <MdHistoryEdu/>
+            </button>
        
         </header>
         <Outlet/>
