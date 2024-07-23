35d717e682b6f8d5070f6249888a0a4271888c6e
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-23 13:49:35 +0330

    display bg image in Home page on md > screens

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Button from '../components/button'
 import { FaArrowRight } from "react-icons/fa";
-import { useState} from "react";
+import { useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
@@ -13,33 +13,40 @@ const Home = () => {
 
     const [location,setLocation]=useState('')
     const [searching,setSarching]=useState(false)
+    const [locationFocus,setLocationFocus]=useState(false)
 
     const navigate=useNavigate()
 
     const handleSearchLocation=async e=>{ 
         e.preventDefault()
         setSarching(true)
-        toast.info('Searching',{autoClose:false,toastId:'isSearching'})
+        toast.info('Searching',{autoClose:false,toastId:'isSearching',closeOnClick:false})
         try {
             const res=await axios(`${URL}appid=${apiKey}&q=${location}&units=metric`)
             navigate("/result", { state:res.data });
         } catch (error) {
             toast.dismiss('isSearching')
-            toast.error('Location Not Found!',{autoClose:4000,draggable:true})            
+            toast.error('Location Not Found!',{autoClose:4000,draggable:true,closeOnClick:true})            
         }
         setSarching(false)
         setLocation('')
        
     }
 
+    useEffect(()=>{
+        console.log(locationFocus)
+    },[locationFocus])
     return (
       
-        <div className='container h-screen  flex justify-center items-center '>
+        <div className='container h-screen  flex justify-center items-center relative '>
+
+            <img 
+            src={process.env.PUBLIC_URL + '/assets/image/homeBG.webp'} 
+            className={`w-full absolute duration-500 blur-[${!locationFocus ? '0px' : '2px'}] hidden md:block`}/>
             <ToastContainer 
                 position='top-center'
                 hideProgressBar={false}
                 newestOnTop={false}
-                closeOnClick={!searching}
                 rtl={false}
                 theme="light"    
             />
@@ -48,6 +55,9 @@ const Home = () => {
             <div className={`relative main-input w-72 h-10  flex justify-between items-center animate__animated animate__infinite ${searching && 'animate__pulse'} bg-gray-800 md:w-96 md:h-16`}>
           
               <input spellCheck type='text' value={location} title='search input'
+              autoFocus
+              onFocus={()=> setLocationFocus(true)}
+              onBlur={()=> setLocationFocus(false)}
               onChange={(prev) => {
                 // disable changing value while searching is in process
                 if(!searching) setLocation(prev.target.value) 
