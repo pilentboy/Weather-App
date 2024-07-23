33640358f56966d701465cf4eed4d2ee333b30c0
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-23 23:39:56 +0330

    responsive mobile home bg img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
 
     return (
       
-        <div className='container h-screen   flex justify-center items-center relative '>
+        <div className='container h-dvh   flex justify-center items-center relative '>
 
             <img 
             src={process.env.PUBLIC_URL + '/assets/image/homeBG.webp'} 
@@ -58,14 +58,10 @@ const Home = () => {
             
             <SlideShow />
 
-         
-            
-          
-        
-
+            <img src={process.env.PUBLIC_URL + '/assets/image/mobileBG2.png'} className={` w-96 h-42  duration-500  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] block md:hidden blur-[${!locationFocus ? '0px' : '2px'}]`} />
 
            <form action='#' onSubmit={handleSearchLocation}>
-            <div className={`relative main-input w-72 h-12   ${selectedLan == 0 ? 'flex flex-row' : 'flex flex-row-reverse'} justify-between items-center animate__animated animate__infinite ${searching && 'animate__pulse'} bg-gray-800 md:w-96 md:h-16`}>
+            <div className={`relative main-input w-72 h-12 z-[500]  ${selectedLan == 0 ? 'flex flex-row' : 'flex flex-row-reverse'} justify-between items-center animate__animated animate__infinite ${searching && 'animate__pulse'} bg-gray-800 md:w-96 md:h-16`}>
 
                 {/* search location input*/}
               <input
