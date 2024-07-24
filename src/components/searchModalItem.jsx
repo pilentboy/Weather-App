41f625811ef3302b66d8b8c940c484be593fd0f4
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-24 14:39:50 +0330

    create searchModalItem

--- src/components/searchModalItem.jsx
+++ src/components/searchModalItem.jsx
@@ -9,18 +9,21 @@ const SearchModalItem = () => {
 
     return (
         <div className="flex justify-around w-full items-center my-2">
+
         <div className="flex items-center  duration-200 cursor-pointer  " >
             <CiLocationOn className="text-white text-2xl" />
             <span className="text-white  ">
                 Rasht
             </span>
         </div>
+
         <button 
         title={selectedLanIndex == 0 ? "delete from your history" : 'حذف از تاریخچه جستجوها'}
         aria-label="delete the item from your search history" 
         className=" text-2xl text-red-600">
             <MdOutlineDelete/>
         </button>
+        
     </div>
     );
 }
