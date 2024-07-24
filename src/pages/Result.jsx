87d36778484c318c0707c215ef56a695e5b7fc60
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-24 18:04:57 +0330

    display localTime location using timezonedb api

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,13 +1,36 @@
 
 import { useLocation } from "react-router-dom";
+import getLocalTimeLocation from "../utils/getLocalTimeLocation";
+import { useEffect, useState } from "react";
 
 const Result = () => {
 
+
     const location =useLocation()
+    const [localTime,setLocalTime]=useState();
 
-    console.log(location.state)
+    async function setLocalTimeLocation(){
+        const res=await  getLocalTimeLocation(location.state.coord)
+        setLocalTime(res)
+    }
 
-    return <h1>{location.state.name}</h1>
+    
+
+    useEffect(()=>{
+        if(!localTime){
+            setLocalTimeLocation()
+        }
+    },[])
+
+    return <>
+    <div className="w-screen h-dvh flex justify-center items-center ">
+
+    <h1>{location.state.name}</h1>
+    <h1 className="text-white text-4xl">{localTime}</h1>
+
+    </div>
+        
+    </>
     
 }
 
