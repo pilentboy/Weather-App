25bda346cd3963777db6d85c1acf67ffd9622842
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-07-24 15:12:53 +0330

    avoiding duplicating locations in localStorage

--- src/utils/addSearchLocationToHistory.js
+++ src/utils/addSearchLocationToHistory.js
@@ -6,7 +6,7 @@ const addSearchLocationToHistory= locationName => {
     if(localStorage.getItem('locationHistory')){
         const updateLocationHistory=JSON.parse(localStorage.getItem('locationHistory'))
         const filterLocations=updateLocationHistory.filter(location => location == upperCaseLocation) // avoiding duplicating locations
-        console.log(filterLocations)
+        
         if(filterLocations.length === 0) {
             updateLocationHistory.push(upperCaseLocation)
             localStorage.setItem('locationHistory',JSON.stringify(updateLocationHistory))
