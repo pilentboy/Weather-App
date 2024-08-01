25536bedeab2996ee85a8f616eb2fd6d6bfe58a4
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-08-02 00:56:55 +0330

    create modalContainer

--- src/utils/addSearchLocationToHistory.js
+++ src/utils/addSearchLocationToHistory.js
@@ -5,7 +5,7 @@ const addSearchLocationToHistory= (locationName,setLocationHistory) => {
     
     if(localStorage.getItem('locationHistory')){
         const updateLocationHistory=JSON.parse(localStorage.getItem('locationHistory'))
-        const filterLocations=updateLocationHistory.filter(location => location == upperCaseLocation) // avoiding duplicating locations
+        const filterLocations=updateLocationHistory.filter(location => location ===  upperCaseLocation) // avoiding duplicating locations
         
         if(filterLocations.length === 0) {
             updateLocationHistory.push(upperCaseLocation)
