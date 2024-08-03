e2b5e14b6700adc546a5da632950bda9b1ab052f
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-08-04 03:11:50 +0330

    add(modalContainer)

---
+++ src/components/modalContainer.jsx
@@ -0,0 +1,13 @@
+
+const ModalContainer = ({children,action,display}) => {
+    return (
+        <div
+        className={`w-screen h-screen z-[1000]  animate__fadeIn animate__animated animate__fast  ${display ? 'flex' : 'hidden'} bg-gray-800 bg-opacity-80 fixed`}
+        onClick={e=> action(e)}
+        >
+            {children}
+        </div>
+    );
+}
+
+export default ModalContainer;
