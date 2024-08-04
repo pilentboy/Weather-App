0809f0c1f03c286327443172293ed504343dd32d
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-08-05 01:20:00 +0330

    change(history modal px + decrease close btn size + add h-screen to home container)

--- src/components/locationHistoryItem.jsx
+++ src/components/locationHistoryItem.jsx
@@ -34,7 +34,7 @@ const LocationHistoryItem = ({location}) => {
 
     return (
         <>
-           <div className="flex justify-between w-3/4 items-center my-2 ">
+           <div className="flex justify-between w-full items-center my-2 ">
 
                 <div 
                 onClick={handleSearchLocationFromHistory}
